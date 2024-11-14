a7cbae5a00269a266e50a81f1c16ec9c5deee759
Gal Klein <gal.klein@post.runi.ac.il>
2024-11-14 09:08:09 +0200

    fix name order

--- Bill3.java
+++ Bill3.java
@@ -2,13 +2,16 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
-	    String name1 = args[2];
+	    String name1 = args[0];
 	    String name2 = args[1];
-		String name3 = args[0];
+		String name3 = args[2];
 		double check = Double.parseDouble(args[3]);
 		double price_each = Math.ceil(check/3);
+
+		String namesPrint = "Dear " + name3 + ", " + name2 + " and " + name1;
+		String pricePrint = ": pay " + price_each + " Shekels each.";
 	
-		System.out.println("Dear "+name3+", "+name2+" and "+
-		name1+": pay "+price_each +" Shekels each.");
+		System.out.println(namesPrint + pricePrint);
+		
 	}
 }
